b2fc7b6379af3c6f8ed1f2e30e9764e95d19a588
Jay55Martinez <martinez.jay@northeastern.edu>
2023-02-11 14:57:11 -0500

    working board

--- board.py
+++ board.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 class Board:
     """
     Class represents the grid where the search algorithm will take place.
@@ -39,23 +37,33 @@ class Board:
     def __init__(self, row_size, col_size):
         self.row_size = row_size
         self.col_size = col_size
-        self.board = [[State.notsearched]*col_size]*row_size
+        self.board = [[State() for _ in range(col_size)]
+                      for _ in range(row_size)
+                      ]
         
     def add_target(self, row, col):
         if row > self.row_size or col > self.col_size:
             raise IndexError("target Index Error")
-        self.board[row][col] = State.target
+        self.board[row][col].make_target()
     
     def add_start(self, row, col):
         if row > self.row_size or col > self.col_size:
             raise IndexError("target Index Error")
-        self.board[row][col] = State.searching
+        self.board[row][col].make_start()
         
     def add_wall(self, row, col):
         if row > self.row_size or col > self.col_size:
             raise IndexError("target Index Error")
-        self.board[row][col] = State.searched
+        self.board[row][col].make_searched()
     
+    def __str__(self):
+        string_board = ''
+        for row in range(self.row_size):
+            for col in range(self.col_size):
+                string_board += str(self.board[row][col])
+            string_board += '\n'
+        return f'' + string_board
+                
 class State:
     """
     class represents the states on the board.
@@ -81,24 +89,56 @@ class State:
     make_not_searched()
         Sets not_searched to true.
         throws error:
-            - if any searched is true
+            - if searched is true
+            
+    make_searched()
+        Sets searched to true.
+        
+    make_searching()
+        Sets searching to true.
+    
+    make_target()
+        Sets target to true and not_searched to true.
+        throws error:
+            - if searched or searching is true
     """
+    
     def __init__(self):
-        self.not_searched = False
         self.searched = False
         self.searching = False
         self.target = False
-    
-    def make_not_searched(self):
-        if self.searched == True:
-            raise ReferenceError('trying to make a searched State not searched')
-        self.not_searched = True
+        self.start = False
     
     def make_searched(self):
         self.searched = True
     
     def make_searching(self):
         self.searching = True
+        self.searched = True
     
     def make_target(self):
         self.target = True
+        
+    def make_start(self):
+        self.start = True
+    
+    def __str__(self):
+        if self.target == True:
+            return f'F'
+        elif self.start == True:
+            return f'S'
+        elif self.searched == True:
+            return f'x'
+        elif self.searching == True:
+            return f'X'
+        else:
+            return f'O'
+
+def main():
+    board = Board(10, 10)
+    board.add_start(0, 0)
+    board.add_target(9, 9)
+    print(board)
+
+if __name__ == "__main__":
+    main()
